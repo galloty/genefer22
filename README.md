90f0185ff6fdb3917013c70bef9309fb9f6c6e21
Yves Gallot <59421144+galloty@users.noreply.github.com>
2022-03-31 13:59:57 +0200

    three-pass transform

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # genefer22
-Generalized Fermat Prime search program 
+Generalized Fermat Prime search program
+
+## About
+
+**genefer22** is an [OpenMP®](https://www.openmp.org/) application.  
+It performs a fast probable primality test for numbers of the form *b*<sup>2<sup>*n*</sup></sup> + 1 with [Fermat test](https://en.wikipedia.org/wiki/Fermat_primality_test).  
+[genefer](https://primes.utm.edu/bios/page.php?id=2740) was created by Yves Gallot in 2001. It has been extensively used by [PrimeGrid](https://www.primegrid.com/forum_forum.php?id=75) computing project.  
+genefer22 is a test application, the final implementation will be included in *genefer*.  
+
+## Build
+
+genefer22 is under development...  
+This version is compiled with gcc and was tested on Windows and Linux (Ubuntu).  
+
+## TODO
+
+- ...
--- src/main.cpp
+++ src/main.cpp
@@ -18,90 +18,204 @@ typedef std::complex<double> Complex;
 
 static Complex rint(const Complex & z) { return Complex(::rint(z.real()), ::rint(z.imag())); }
 
-static void forward(Complex * const z, const size_t n, const Complex * const w)
+static void forward_out(Complex * const z, const size_t n, const size_t m_io, const Complex * const w)
 {
-	for (size_t m = n / 2, s = 1; m > 0; m /= 2, s *= 2)
+	for (size_t l = 0; l < m_io; ++l)
 	{
-		for (size_t j = 0; j < s; ++j)
+		for (size_t m = n / 2, s = 1; m >= m_io; m /= 2, s *= 2)
 		{
-			const Complex r = w[s + j];
-			for (size_t i = 0; i < m; ++i)
+			for (size_t j = 0; j < s; ++j)
 			{
-				const size_t k = 2 * m * j + i;
-				const Complex u = z[k], v = z[k + m] * r;
-				z[k] = u + v; z[k + m] = u - v;
+				const Complex r = w[s + j];
+				for (size_t i = 0; i < m; i += m_io)
+				{
+					const size_t k = 2 * m * j + i + l;
+					const Complex u = z[k], v = z[k + m] * r;
+					z[k] = u + v; z[k + m] = u - v;
+				}
 			}
 		}
 	}
 }
 
-static void backward(Complex * const z, const size_t n, const Complex * const w)
+static void backward_out(Complex * const z, const size_t n, const size_t m_io, const Complex * const w)
 {
-	for (size_t m = 1, s = n / 2; m <= n / 2; m *= 2, s /= 2)
+	const size_t s_io = (n / 2) / m_io;
+
+	for (size_t l = 0; l < m_io; ++l)
 	{
-		for (size_t j = 0; j < s; ++j)
+		for (size_t m = m_io, s = s_io; m <= n / 2; m *= 2, s /= 2)
 		{
-			const Complex r = std::conj(w[s + j]);
-			for (size_t i = 0; i < m; ++i)
+			for (size_t j = 0; j < s; ++j)
 			{
-				const size_t k = 2 * m * j + i;
-				const Complex u = z[k], v = z[k + m];
-				z[k] = u + v; z[k + m] = (u - v) * r;
+				const Complex r = std::conj(w[s + j]);
+				for (size_t i = 0; i < m; i += m_io)
+				{
+					const size_t k = 2 * m * j + i + l;
+					const Complex u = z[k], v = z[k + m];
+					z[k] = u + v; z[k + m] = (u - v) * r;
+				}
 			}
 		}
 	}
 }
 
-static void square(Complex * const z, const size_t n)
+static void pass1(Complex * const z, const size_t n, const size_t m_io_2, const Complex * const w)
 {
-	for (size_t k = 0; k < n; ++k) z[k] *= z[k];
+	const size_t s_io_2 = (n / 2) / m_io_2;
+
+	for (size_t l = 0; l < s_io_2; ++l)	// parallel
+	{
+		// forward_in
+		for (size_t m = m_io_2, s = 1; m >= 1; m /= 2, s *= 2)
+		{
+			for (size_t j = 0; j < s; ++j)
+			{
+				const Complex r = w[(s_io_2 + l) * s + j];
+				for (size_t i = 0; i < m; ++i)
+				{
+					const size_t k = 2 * m_io_2 * l + 2 * m * j + i;
+					const Complex u = z[k], v = z[k + m] * r;
+					z[k] = u + v; z[k + m] = u - v;
+				}
+			}
+		}
+
+		// square
+		for (size_t j = 0; j < 2 * m_io_2; ++j)
+		{
+			const size_t k = 2 * m_io_2 * l + j;
+			z[k] *= z[k];
+		}
+
+		// backward_in
+		for (size_t m = 1, s = m_io_2; m <= m_io_2; m *= 2, s /= 2)
+		{
+			for (size_t j = 0; j < s; ++j)
+			{
+				const Complex r = std::conj(w[(s_io_2 + l) * s + j]);
+				for (size_t i = 0; i < m; ++i)
+				{
+					const size_t k = 2 * m_io_2 * l + 2 * m * j + i;
+					const Complex u = z[k], v = z[k + m];
+					z[k] = u + v; z[k + m] = (u - v) * r;
+				}
+			}
+		}
+	}
 }
 
-static double reduceDup(Complex * const z, const size_t n, const bool dup, const double b, const double sb, const double isb, const double fsb)
+static double pass2(Complex * const z, const size_t n, const size_t m_io, const Complex * const w, Complex * const f,
+					const bool dup, const double b, const double sb, const double isb, const double fsb)
 {
+	const size_t s_io = (n / 2) / m_io;
 	const double b_inv = 1.0 / b, sb_inv = 1.0 / sb, n_inv = 1.0 / n, g = dup ? 2 : 1;
 
 	double err = 0;
-	Complex f = Complex(0, 0);
-	for (size_t k = 0; k < n; k += 2)
+	for (size_t lh = 0; lh < m_io / 2 / 4; ++lh)	// parallel
 	{
-		const Complex o = n_inv * (z[k] + sb * z[k + 1]), oi = rint(o), d = fabs(o - oi);
-		f += g * oi;
-		err = std::max(err, std::max(d.real(), d.imag()));
-		const Complex f_b = rint(f * b_inv);
-		const Complex r = f - f_b * b;
-		f = f_b;
-		const Complex irh = rint(r * sb_inv);
-		z[k] = (r - isb * irh) - fsb * irh;
-		z[k + 1] = irh;
-	}
-	while (f != Complex(0, 0))
-	{
-		f = Complex(-f.imag(), f.real());
-		for (size_t k = 0; k < n; k += 2)
+		// backward_out
+		for (size_t ll = 0; ll < 8; ++ll)
 		{
-			const Complex o = z[k] + sb * z[k + 1], oi = rint(o);
-			f += oi;
-			const Complex f_b = rint(f * b_inv);
-			const Complex r = f - f_b * b;
-			f = f_b;
-			const Complex irh = rint(r * sb_inv);
-			z[k] = (r - isb * irh) - fsb * irh;
-			z[k + 1] = irh;
-			if (f == Complex(0, 0)) break;
+			const size_t l = 8 * lh + ll;
+			for (size_t m = m_io, s = s_io; m <= n / 2; m *= 2, s /= 2)
+			{
+				for (size_t j = 0; j < s; ++j)
+				{
+					const Complex r = std::conj(w[s + j]);
+					for (size_t i = 0; i < m; i += m_io)
+					{
+						const size_t k = 2 * m * j + i + l;
+						const Complex u = z[k], v = z[k + m];
+						z[k] = u + v; z[k + m] = (u - v) * r;
+					}
+				}
+			}
+		}
+
+		// carry_out
+		for (size_t ll = 0; ll < 4; ++ll)
+		{
+			for (size_t j = 0; j < (n / 2) / (m_io / 2); ++j)
+			{
+				const size_t k_4 = (m_io / 2) / 4 * j + lh;
+				const size_t k = 4 * k_4 + ll;
+				const Complex o = n_inv * (z[2 * k + 0] + sb * z[2 * k + 1]), oi = rint(o), d = fabs(o - oi);
+				const Complex f_i = f[k_4] + g * oi;
+				err = std::max(err, std::max(d.real(), d.imag()));
+				const Complex f_o = rint(f_i * b_inv);
+				const Complex r = f_i - f_o * b;
+				f[k_4] = f_o;
+				const Complex irh = rint(r * sb_inv);
+				z[2 * k + 0] = (r - isb * irh) - fsb * irh;
+				z[2 * k + 1] = irh;
+			}
 		}
 	}
 	return err;
 }
 
-static void reducePos(Complex * const z, const size_t n, const uint32_t b, const double sb)
+static void pass3(Complex * const z, const size_t n, const size_t m_io, const Complex * const w, Complex * const f,
+				  const double b, const double sb, const double isb, const double fsb)
 {
+	const double b_inv = 1.0 / b, sb_inv = 1.0 / sb;
+
+	for (size_t lh = 0; lh < m_io / 2 / 4; ++lh)	// parallel
+	{
+		// carry_in
+		for (size_t j = 0; j < (n / 2) / (m_io / 2); ++j)
+		{
+			const size_t k_4 = (m_io / 2) / 4 * j + lh;
+			const size_t carry_i = (k_4 == 0) ? n / 2 / 4 - 1 : k_4 - 1;
+			Complex ff = (k_4 == 0) ? Complex(-f[carry_i].imag(), f[carry_i].real()) : f[carry_i];
+			f[carry_i] = Complex(0, 0);
+			for (size_t ll = 0; ll < 4; ++ll)
+			{
+				const size_t k = 4 * k_4 + ll;
+				const Complex o = z[2 * k + 0] + sb * z[2 * k + 1], oi = rint(o);
+				ff += oi;
+				const Complex f_o = rint(ff * b_inv);
+				const Complex r = ff - f_o * b;
+				ff = f_o;
+				const Complex irh = rint(r * sb_inv);
+				z[2 * k + 0] = (r - isb * irh) - fsb * irh;
+				z[2 * k + 1] = irh;
+				if (ff == Complex(0, 0)) break;
+			}
+			if (ff != Complex(0, 0)) std::cout << "Error!" << std::endl;	// TODO
+		}
+
+		// forward_out
+		for (size_t ll = 0; ll < 8; ++ll)
+		{
+			const size_t l = 8 * lh + ll;
+			for (size_t m = n / 2, s = 1; m >= m_io; m /= 2, s *= 2)
+			{
+				for (size_t j = 0; j < s; ++j)
+				{
+					const Complex r = w[s + j];
+					for (size_t i = 0; i < m; i += m_io)
+					{
+						const size_t k = 2 * m * j + i + l;
+						const Complex u = z[k], v = z[k + m] * r;
+						z[k] = u + v; z[k + m] = u - v;
+					}
+				}
+			}
+		}
+	}
+}
+
+static void reducePos(Complex * const z, const size_t n, const size_t n_io, const uint32_t b, const double sb)
+{
+	const double n_io_inv = n_io / double(n);
+
 	int64_t f = 0;
 	for (size_t k = 0; k < 2 * n; k += 2)
 	{
 		const double o1 = (k < n) ? z[k].real() : z[k - n].imag();
 		const double o2 = (k < n) ? z[k + 1].real() : z[k + 1 - n].imag();
-		const double o = o1 + sb * o2;
+		const double o = (o1 + sb * o2) * n_io_inv;
 		f += llrint(o);
 		int32_t r = f % b; if (r < 0) r += b;
 		f -= r; f /= b;
@@ -126,9 +240,12 @@ static void reducePos(Complex * const z, const size_t n, const uint32_t b, const
 class transform
 {
 private:
+	const size_t n_io = 32;
+
 	const size_t _n;
 	Complex * const _w;
 	Complex * const _z;
+	Complex * const _f;
 	const uint32_t _b;
 	double _sb, _isb, _fsb;
 
@@ -141,7 +258,7 @@ private:
 	}
 
 public:
-	transform(const size_t n, const uint32_t b) : _n(n), _w(new Complex[n]), _z(new Complex[n]), _b(b)
+	transform(const size_t n, const uint32_t b) : _n(n), _w(new Complex[n]), _z(new Complex[n]), _f(new Complex[n / 2 / 4]), _b(b)
 	{
 		Complex * const w = _w;
 		for (size_t s = 1; s < n; s *= 2)
@@ -158,8 +275,13 @@ public:
 		z[0] = Complex(2, 0);
 		for (size_t k = 1; k < n; ++k) z[k] = Complex(0, 0);
 
+		Complex * const f = _f;
+		for (size_t k = 0; k < n / 2 / 4; ++k) f[k] = Complex(0, 0);
+
 		const fp16_80 sqrt_b = fp16_80::sqrt(b);
 		_sb = double(sqrtl(b)); _isb = sqrt_b.hi(); _fsb = sqrt_b.lo();
+
+		forward_out(_z, _n, n_io, _w);
 	}
 
 	virtual ~transform()
@@ -170,15 +292,16 @@ public:
 
 	double squareDup(const bool dup)
 	{
-		forward(_z, _n, _w);
-		square(_z, _n);
-		backward(_z, _n, _w);
-		return reduceDup(_z, _n, dup, double(_b), _sb, _isb, _fsb);
+		pass1(_z, _n, n_io / 2, _w);
+		const double err = pass2(_z, _n, n_io, _w, _f, dup, double(_b), _sb, _isb, _fsb);
+		pass3(_z, _n, n_io, _w, _f, double(_b), _sb, _isb, _fsb);
+		return err;
 	}
 
 	bool isPrime()
 	{
-		reducePos(_z, _n, _b, _sb);
+		backward_out(_z, _n, n_io, _w);
+		reducePos(_z, _n, n_io, _b, _sb);
 
 		Complex * const z = _z;
 		bool isPrime = (z[0] == Complex(1, 0));
@@ -201,14 +324,13 @@ public:
 		const integer exponent(b, n);
 
 		double err = 0;
-		for (size_t i = exponent.bitSize() - 1; i != 0; --i)
+		for (int i = int(exponent.bitSize()) - 1; i >= 0; --i)
 		{
 			const double e = t.squareDup(exponent.bit(i));
 			err  = std::max(err, e);
 		}
-		const double e = t.squareDup(exponent.bit(0));
-		maxError = std::max(err, e);
 
+		maxError = err;
 		return t.isPrime();
 	}
 };
@@ -223,8 +345,9 @@ int main(/*int argc, char * argv[]*/)
 		{
 			double maxError = 1;
 			const bool isPrime = g.check(b, n, maxError);
-			if (isPrime) std::cout << b << "*, " << maxError << std::endl;
-			else std::cout << b << ", " << maxError << std::endl;
+			std::cout << b;
+			if (isPrime) std::cout << "*";
+			std::cout << ", " << maxError << std::endl;
 		}
 	}
 	catch (const std::runtime_error & e)

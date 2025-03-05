8e0f720157b5218984bf9d5a97c8fca8f8292b49
galloty <galloty@orange.fr>
2025-03-05 12:21:25 +0100

    Galois NTT: opt for a 32-bit unit

--- design/galois.cpp
+++ design/galois.cpp
@@ -17,29 +17,41 @@ class Zp
 {
 private:
 	static const uint64_t _p = (uint64_t(1) << 61) - 1;
-	uint64_t _n;
+	uint64_t _n;	// 0 <= n <= p
 
-	static uint64_t add(const uint64_t a, const uint64_t b) { return a + b - ((a >= _p - b) ? _p : 0); }
-	static uint64_t sub(const uint64_t a, const uint64_t b) { return a - b + ((a < b) ? _p : 0); }
-	static uint64_t mul(const uint64_t a, const uint64_t b)
+	static uint64_t _add(const uint64_t a, const uint64_t b)
+	{
+		const uint64_t t = a + b;
+		const uint64_t c = (uint32_t(t >> 32) > uint32_t(_p >> 32)) ? _p : 0;	// t > p ?
+		return t - c;
+	}
+
+	static uint64_t _sub(const uint64_t a, const uint64_t b)
+	{
+		const uint64_t t = a - b;
+		const uint64_t c = (int32_t(t >> 32) < 0) ? _p : 0;	// t < 0 ?
+		return t + c;
+	}
+
+	static uint64_t _mul(const uint64_t a, const uint64_t b)
 	{
 		const __uint128_t t = a * __uint128_t(b);
 		const uint64_t lo = uint64_t(t) & _p, hi = uint64_t(t >> 61);
-		return add(hi, lo);
+		return _add(hi, lo);
 	}
 
 public:
 	Zp() {}
 	explicit Zp(const uint64_t n) : _n(n) {}
 
-	int64_t get_int() const { return (_n >= _p / 2) ? int64_t(_n - _p) : int64_t(_n); }
-	Zp & set_int(const int64_t i) { _n = (i < 0) ? _p + uint64_t(i) : uint64_t(i); return *this; }
+	int64_t get_int() const { return (_n >= _p / 2) ? int64_t(_n - _p) : int64_t(_n); }	// if n = p then return 0
+	Zp & set_int(const int64_t i) { _n = (i < 0) ? uint64_t(i) + _p : uint64_t(i); return *this; }
 
-	// Zp operator-() const { return Zp((_n == 0) ? 0 : _p - _n); }
+	// Zp neg() const { return Zp((_n == 0) ? 0 : _p - _n); }
 
-	Zp operator+(const Zp & rhs) const { return Zp(add(_n, rhs._n)); }
-	Zp operator-(const Zp & rhs) const { return Zp(sub(_n, rhs._n)); }
-	Zp operator*(const Zp & rhs) const { return Zp(mul(_n, rhs._n)); }
+	Zp add(const Zp & rhs) const { return Zp(_add(_n, rhs._n)); }
+	Zp sub(const Zp & rhs) const { return Zp(_sub(_n, rhs._n)); }
+	Zp mul(const Zp & rhs) const { return Zp(_mul(_n, rhs._n)); }
 
 	static Zp inv(const uint64_t n) { return Zp((_p + 1) / n); }
 };
@@ -62,27 +74,24 @@ public:
 
 	// GF conj() const { return GF(_a, -_b); }
 
-	GF operator+(const GF & rhs) const { return GF(_a + rhs._a, _b + rhs._b); }
-	GF operator-(const GF & rhs) const { return GF(_a - rhs._a, _b - rhs._b); }
-	GF operator*(const Zp & rhs) const { return GF(_a * rhs, _b * rhs); }
+	GF add(const GF & rhs) const { return GF(_a.add(rhs._a), _b.add(rhs._b)); }
+	GF sub(const GF & rhs) const { return GF(_a.sub(rhs._a), _b.sub(rhs._b)); }
+	GF muls(const Zp & rhs) const { return GF(_a.mul(rhs), _b.mul(rhs)); }
 
-	GF sqr() const { const Zp t = _a * _b; return GF(_a * _a - _b * _b, t + t); }
-	GF mul(const GF & rhs) const { return GF(_a * rhs._a - _b * rhs._b, _b * rhs._a + _a * rhs._b); }
-	GF mulconj(const GF & rhs) const { return GF(_a * rhs._a + _b * rhs._b, _b * rhs._a - _a * rhs._b); }
-
-	GF & operator*=(const GF & rhs) { *this = GF(_a * rhs._a - _b * rhs._b, _b * rhs._a + _a * rhs._b); return *this; }
+	GF sqr() const { const Zp t = _a.mul(_b); return GF(_a.mul(_a).sub(_b.mul(_b)), t.add(t)); }
+	GF mul(const GF & rhs) const { return GF(_a.mul(rhs._a).sub(_b.mul(rhs._b)), _b.mul(rhs._a).add(_a.mul(rhs._b))); }
+	GF mulconj(const GF & rhs) const { return GF(_a.mul(rhs._a).add(_b.mul(rhs._b)), _b.mul(rhs._a).sub(_a.mul(rhs._b))); }
 
 	// GF muli() const { return GF(-_b, _a); }
-	GF addi(const GF & rhs) const { return GF(_a - rhs._b, _b + rhs._a); }
-	GF subi(const GF & rhs) const { return GF(_a + rhs._b, _b - rhs._a); }
+	GF addi(const GF & rhs) const { return GF(_a.sub(rhs._b), _b.add(rhs._a)); }
+	GF subi(const GF & rhs) const { return GF(_a.add(rhs._b), _b.sub(rhs._a)); }
 
 	GF pow(const uint64_t e) const
 	{
 		if (e == 0) return GF(Zp(1), Zp(0));
 		GF r = GF(Zp(1), Zp(0)), y = *this;
-		for (uint64_t i = e; i != 1; i /= 2) { if (i % 2 != 0) r *= y; y = y.sqr(); }
-		r *= y;
-		return r;
+		for (uint64_t i = e; i != 1; i /= 2) { if (i % 2 != 0) r = r.mul(y); y = y.sqr(); }
+		return r.mul(y);
 	}
 
 	static const GF primroot_n(const uint64_t n) { return GF(Zp(_h_a), Zp(_h_b)).pow(_h_order / n); }
@@ -94,7 +103,8 @@ private:
 	std::vector<GF> _vz;
 	std::vector<GF> _vwr;
 	const int32_t _base;
-	const Zp _multiplier;
+	const int32_t _multiplier;
+	const Zp _norm;
 
 private:
 	static constexpr size_t bitrev(const size_t i, const size_t n)
@@ -104,41 +114,41 @@ private:
 		return r;
 	}
 
-	void forward2(const size_t m, const size_t s)
-	{
-		GF * const z = _vz.data();
-		const GF * const wr = _vwr.data();
+	// void forward2(const size_t m, const size_t s)
+	// {
+	// 	GF * const z = _vz.data();
+	// 	const GF * const wr = _vwr.data();
 
-		for (size_t j = 0; j < s; ++j)
-		{
-			const GF w = wr[s + j];
+	// 	for (size_t j = 0; j < s; ++j)
+	// 	{
+	// 		const GF w = wr[s + j];
 
-			for (size_t i = 0; i < m; ++i)
-			{
-				const size_t k = 2 * m * j + i;
-				const GF u0 = z[k + 0 * m], u1 = z[k + 1 * m].mul(w);
-				z[k + 0 * m] = u0 + u1; z[k + 1 * m] = u0 - u1;
-			}
-		}
-	}
+	// 		for (size_t i = 0; i < m; ++i)
+	// 		{
+	// 			const size_t k = 2 * m * j + i;
+	// 			const GF u0 = z[k + 0 * m], u1 = z[k + 1 * m].mul(w);
+	// 			z[k + 0 * m] = u0.add(u1); z[k + 1 * m] = u0.sub(u1);
+	// 		}
+	// 	}
+	// }
 
-	void backward2(const size_t m, const size_t s)
-	{
-		GF * const z = _vz.data();
-		const GF * const wr = _vwr.data();
+	// void backward2(const size_t m, const size_t s)
+	// {
+	// 	GF * const z = _vz.data();
+	// 	const GF * const wr = _vwr.data();
 
-		for (size_t j = 0; j < s; ++j)
-		{
-			const GF w = wr[s + j];
+	// 	for (size_t j = 0; j < s; ++j)
+	// 	{
+	// 		const GF w = wr[s + j];
 
-			for (size_t i = 0; i < m; ++i)
-			{
-				const size_t k = 2 * m * j + i;
-				const GF u0 = z[k + 0 * m], u1 = z[k + 1 * m];
-				z[k + 0 * m] = u0 + u1; z[k + 1 * m] = (u0 - u1).mulconj(w);
-			}
-		}
-	}
+	// 		for (size_t i = 0; i < m; ++i)
+	// 		{
+	// 			const size_t k = 2 * m * j + i;
+	// 			const GF u0 = z[k + 0 * m], u1 = z[k + 1 * m];
+	// 			z[k + 0 * m] = u0.add(u1); z[k + 1 * m] = u0.sub(u1).mulconj(w);
+	// 		}
+	// 	}
+	// }
 
 	void forward4(const size_t m, const size_t s)
 	{
@@ -153,8 +163,8 @@ private:
 			{
 				const size_t k = 4 * m * j + i;
 				const GF u0 = z[k + 0 * m], u1 = z[k + 1 * m].mul(w2), u2 = z[k + 2 * m].mul(w1), u3 = z[k + 3 * m].mul(w3);
-				const GF v0 = u0 + u2, v1 = u1 + u3, v2 = u0 - u2, v3 = u1 - u3;
-				z[k + 0 * m] = v0 + v1; z[k + 1 * m] = v0 - v1;
+				const GF v0 = u0.add(u2), v1 = u1.add(u3), v2 = u0.sub(u2), v3 = u1.sub(u3);
+				z[k + 0 * m] = v0.add(v1); z[k + 1 * m] = v0.sub(v1);
 				z[k + 2 * m] = v2.addi(v3); z[k + 3 * m] = v2.subi(v3);
 			}
 		}
@@ -173,8 +183,8 @@ private:
 			{
 				const size_t k = 4 * m * j + i;
 				const GF u0 = z[k + 0 * m], u1 = z[k + 1 * m], u2 = z[k + 2 * m], u3 = z[k + 3 * m];
-				const GF v0 = u0 + u1, v1 = u0 - u1, v2 = u2 + u3, v3 = u3 - u2;
-				z[k + 0 * m] = v0 + v2; z[k + 2 * m] = (v0 - v2).mulconj(w1);
+				const GF v0 = u0.add(u1), v1 = u0.sub(u1), v2 = u2.add(u3), v3 = u3.sub(u2);
+				z[k + 0 * m] = v0.add(v2); z[k + 2 * m] = v0.sub(v2).mulconj(w1);
 				z[k + 1 * m] = v1.addi(v3).mulconj(w2); z[k + 3 * m] = v1.subi(v3).mulconj(w3);
 			}
 		}
@@ -186,17 +196,14 @@ private:
 		GF * const z = _vz.data();
 		const GF * const wr = _vwr.data();
 
-		for (size_t j = 0; j < n / 8; ++j)
+		for (size_t j = 0; j < n / 4; ++j)
 		{
-			const GF w2 = wr[n / 2 + 4 * j].mul(wr[n / 2 + 4 * j]);
+			const GF w = wr[n / 4 + j];
 
-			const size_t k = 8 * j;
+			const size_t k = 4 * j;
 			const GF u0 = z[k + 0], u1 = z[k + 1], u2 = z[k + 2], u3 = z[k + 3];
-			z[k + 0] = u0.sqr() + u1.sqr().mul(w2); z[k + 1] = u0.mul(u1 + u1);
-			z[k + 2] = u2.sqr() - u3.sqr().mul(w2); z[k + 3] = u2.mul(u3 + u3);
-			const GF u4 = z[k + 4], u5 = z[k + 5], u6 = z[k + 6], u7 = z[k + 7];
-			z[k + 4] = u4.sqr().addi(u5.sqr().mul(w2)); z[k + 5] = u4.mul(u5 + u5);
-			z[k + 6] = u6.sqr().subi(u7.sqr().mul(w2)); z[k + 7] = u6.mul(u7 + u7);
+			z[k + 0] = u0.sqr().add(u1.sqr().mul(w)); z[k + 1] = u0.mul(u1.add(u1));
+			z[k + 2] = u2.sqr().sub(u3.sqr().mul(w)); z[k + 3] = u2.mul(u3.add(u3));
 		}
 	}
 
@@ -206,27 +213,18 @@ private:
 		GF * const z = _vz.data();
 		const GF * const wr = _vwr.data();
 
-		for (size_t j = 0; j < n / 8; ++j)
+		for (size_t j = 0; j < n / 4; ++j)
 		{
-			const GF w1 = wr[n / 4 + 2 * j];
-			const GF w2 = wr[n / 2 + 4 * j].mul(wr[n / 2 + 4 * j]);
+			const GF w = wr[n / 4 + j];
 
-			const size_t k = 8 * j;
-			const GF u0 = z[k + 0], u1 = z[k + 1], u2 = z[k + 2].mul(w1), u3 = z[k + 3].mul(w1);
-			const GF v0 = u0 + u2, v1 = u1 + u3, v2 = u0 - u2, v3 = u1 - u3;
-			const GF s0 = v0.sqr() + v1.sqr().mul(w2), s1 = v0.mul(v1 + v1);
-			const GF s2 = v2.sqr() - v3.sqr().mul(w2), s3 = v2.mul(v3 + v3);
-			z[k + 0] = s0 + s2; z[k + 2] = (s0 - s2).mulconj(w1);
-			z[k + 1] = s1 + s3; z[k + 3] = (s1 - s3).mulconj(w1);
-
-			const GF u4 = z[k + 4], u5 = z[k + 5], u6 = z[k + 6].mul(w1), u7 = z[k + 7].mul(w1);
-			const GF v4 = u4.addi(u6), v5 = u5.addi(u7), v6 = u6.addi(u4), v7 = u5.subi(u7);
-			const GF s4 = v4.sqr().addi(v5.sqr().mul(w2)), s5 = v4.mul(v5 + v5);
-			const GF s6 = v7.sqr().mul(w2).subi(v6.sqr()), s7 = v6.mul(v7 + v7);
-			z[k + 4] = s4.subi(s6); z[k + 6] = s6.subi(s4).mulconj(w1);
-			z[k + 5] = s5.subi(s7); z[k + 7] = s7.subi(s5).mulconj(w1);
+			const size_t k = 4 * j;
+			const GF u0 = z[k + 0], u1 = z[k + 1], u2 = z[k + 2].mul(w), u3 = z[k + 3].mul(w);
+			const GF v0 = u0.add(u2), v1 = u1.add(u3), v2 = u0.sub(u2), v3 = u1.sub(u3);
+			const GF s0 = v0.sqr().add(v1.sqr().mul(w)), s1 = v0.mul(v1.add(v1));
+			const GF s2 = v2.sqr().sub(v3.sqr().mul(w)), s3 = v2.mul(v3.add(v3));
+			z[k + 0] = s0.add(s2); z[k + 2] = s0.sub(s2).mulconj(w);
+			z[k + 1] = s1.add(s3); z[k + 3] = s1.sub(s3).mulconj(w);
 		}
-
 	}
 
 	void carry(const bool mul)
@@ -234,15 +232,16 @@ private:
 		const size_t n = _vz.size();
 		GF * const z = _vz.data();
 
-		const Zp r = Zp::inv(n / 2), rg = mul ? r * _multiplier : r;
-
-		const int32_t base = _base;
+		const Zp norm = _norm;
+		const int32_t m = _multiplier, base = _base;
 		int64_t fa = 0, fb = 0;
 
 		for (size_t k = 0; k < n; ++k)
 		{
-			const GF u = z[k] * rg;
-			fa += u.a().get_int(); fb += u.b().get_int();
+			const GF u = z[k].muls(norm);
+			int64_t ia = u.a().get_int(), ib = u.b().get_int();
+			if (mul) { ia *= m; ib *= m; }
+			fa += ia; fb += ib;
 			int64_t la = fa / base, lb = fb / base;
 			z[k] = GF(Zp().set_int(fa - la * base), Zp().set_int(fb - lb * base));
 			fa = la; fb = lb;
@@ -259,19 +258,19 @@ private:
 				int64_t la = fa / base, lb = fb / base;
 				z[k] = GF(Zp().set_int(fa - la * base), Zp().set_int(fb - lb * base));
 				fa = la; fb = lb;
-				if ((fa == 0) && (fb == 0)) break;
+				if ((la == 0) && (lb == 0)) break;
 			}
 		}
 	}
 
 public:
 	Transform(const size_t size, const uint32_t b, const uint32_t a)
-		: _vz(size / 2, GF(Zp(0), Zp(0))), _vwr(size / 2), _base(int32_t(b)), _multiplier(a)
+		: _vz(size / 2, GF(Zp(0), Zp(0))), _vwr(size / 4), _base(int32_t(b)), _multiplier(int32_t(a)), _norm(Zp::inv(size / 4))
 	{
 		const size_t n = _vz.size();
 		GF * const wr = _vwr.data();
 
-		for (size_t s = 1; s < n; s *= 2)
+		for (size_t s = 1; s < n / 2; s *= 2)
 		{
 			const GF r_s = GF::primroot_n(2 * 4 * s);
 			for (size_t j = 0; j < s; ++j)
